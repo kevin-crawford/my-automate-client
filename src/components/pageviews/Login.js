3f964aa4bddd2cfb1a0a50bfb34873550dab0727
Kevin Crawford <kcdouglas92@gmail.com>
2018-12-02 16:51:09 -0800

    added login componenent lifecycle

--- src/components/pageviews/Login.js
+++ src/components/pageviews/Login.js
@@ -1,8 +1,28 @@
 import React from "react";
+import {Field, reduxForm, focus} from 'redux-form';
+import Input from './Input';
+import {login} from '../../actions/auth';
+import {required, nonEmpty} from '../../validators';
+import  {Link} from 'react-router-dom';
 
 import './Login.css'
 
-export default function Login() {
+export class LoginForm extends React.Component {
+	onSubmit(values){
+		return this.props
+		.dispatch(login(values.email, values.password))
+		.then(() => this.props.history.push(`/garage`));
+	}
+
+	render(){
+		let error;
+		if(this.props.error) {
+			error = (
+				<div className="form-error" aria-live="polite">
+					{this.props.error}   
+				</div>
+			)
+		}
 		return(
 			<div className="Login">
 				<form id="login-form" className="login-form">
@@ -15,4 +35,5 @@ export default function Login() {
 				</form>
 			</div>
 		)
+	}
 };
